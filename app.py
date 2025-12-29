c9af2b5407c8274e61721f25b9ac33f98b05379e
codebyte0 <codebyte.wd@gmail.com>
2025-12-30 02:25:41 +0500

    remove debug line for azure dev

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,5 +15,6 @@ def about():
 def membership():
     return render_template("membership.html")
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
